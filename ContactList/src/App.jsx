dc95c8bbaef9978ed52b5d0cbb93a74992077474
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-09-09 01:16:30 -0400

    Set up contacts and put them in a table to display neatly on the webpage

--- ContactList/src/App.jsx
+++ ContactList/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList.jsx'
 
 function App() {
 
 
   return (
     <>
-      
+      <ContactList />
     </>
   )
 }
